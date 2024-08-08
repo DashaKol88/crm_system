9f75cadfd6f51fa6fd87c5bcbfc0c7f6afff22ac
DashaKol88 <dashytamakarenko88@gmail.com>
2024-08-08 15:07:16 +0300

    create html for client_list and client_detail

---
+++ crm_project/clients/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+
+class Client(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email = models.EmailField()
+    phone = models.CharField(max_length=15)
+    address = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
